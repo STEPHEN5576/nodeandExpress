ce787f4428e39243445b04fdb7287ff97b821e1f
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-03-14 16:57:54 +0530

    commited

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,8 +1,20 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  console.log(req);
-  res.writeHead(200, { "content-type": "text/html" });
-  res.write("<h1>Home Page</h1>");
-  res.end();
+  const url = req.url;
+
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>Home Page</h1>");
+    res.end();
+  } 
+  else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>About Page</h1>");
+  }
+  else{
+   res.writeHead(404, {"content-type" : "text/html" });
+   res.write("<h1>Home Page</h1>");
+   res.end();
+  }
 });
 server.listen(5000);
