30316a12c0b8c22e19b6d5fb034e319fb114eb95
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-03-15 18:52:31 +0530

    commited

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const app = express();
-const {products}= require('./data');
+const { products } = require("./data");
 app.set("json spaces", 2);
 app.get("/", (req, res) => {
   res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
 });
-app.get("/api/products", (req , res) =>{
-  const newProducts = products.map((product) =>{
-    const {id, name , image} = product;
-    return {id, name, image};
-  })
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
   res.json(newProducts);
-})
+});
+app.get("/api/products/1", (req, res) => {
+  const singleproduct = products.find((product) => product.id === 1);
+  res.json(singleproduct);
+});
+
 app.listen(5000, () => {
   console.log("Server is listening to 5000...");
 });
