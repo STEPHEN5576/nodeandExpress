916a25f2a1d2e41c2098c316173dff0b6998fffa
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-03-15 17:09:17 +0530

    comitted

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -4,9 +4,9 @@ const path = require("path");
 // setup static and middleware
 app.use(express.static('./public'))
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+// });
 
 app.get("*", (req, res) => {
   res.status(404).send("resource not found");
