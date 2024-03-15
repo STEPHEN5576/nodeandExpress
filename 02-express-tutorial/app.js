843d69a79cdbd1bdf1f4b34b1956a47ab08f852e
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-03-15 15:45:18 +0530

    commited

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,21 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.use(express.static('./public'))
 app.get("/", (req, res) => {
-  res.status(200).send("Home Page");
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
 });
-app.get("/about", (req, res) => {
-  res.status(200).send("About Page");
+
+app.get("*", (req, res) => {
+  res.status(404).send("resource not found");
 });
-app.all("*", (req, res) => {
-  res.status(404).send("<h1>Resourse not found</h1>");
-});
+
 app.listen(5000, () => {
-  console.log("the server is listening on port 5000....");
+  console.log("server started in 5000");
 });
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
