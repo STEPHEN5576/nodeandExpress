35a9c2a94c89b3dc38d40d55b24fe1486ffa6f38
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-03-15 11:51:04 +0530

    commited

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,45 +1,21 @@
-const { readFileSync } = require("fs");
-const http = require("http");
-
-const HomePage = readFileSync("./navbar-app/index.html");
-const styles = readFileSync("./navbar-app/styles.css");
-const image = readFileSync("./navbar-app/logo.svg");
-const logic = readFileSync("./navbar-app/browser-app.js");
-
-const server = http.createServer((req, res) => {
-  const url = req.url;
-
-  // Home Page
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(HomePage);
-    res.end();
-  }
-  // About Page
-  else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>About Page</h1>");
-    res.end();
-  }
-  // Style Page
-  else if (url === "/styles.css") {
-    res.writeHead(200, { "content-type": "text/css" });
-    res.write(styles);
-    res.end();
-  }
-  // svg
-  else if (url === "/logo.svg") {
-    res.writeHead(200, { "content-type": "image/svg+xml" });
-    res.write(image);
-    res.end();
-  } else if (url === "/browser-app.js") {
-    res.writeHead(200, { "content-type": "text/javascript" });
-    res.write(logic);
-    res.end();
-  } else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.write("<h1>Home Page</h1>");
-    res.end();
-  }
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.status(200).send("Home Page");
 });
-server.listen(5000);
+app.get("/about", (req, res) => {
+  res.status(200).send("About Page");
+});
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>Resourse not found</h1>");
+});
+app.listen(5000, () => {
+  console.log("the server is listening on port 5000....");
+});
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
