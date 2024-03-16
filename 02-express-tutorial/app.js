cbfcce0bc3aba56d51600dad681f611a0b34ebfb
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-03-16 19:10:28 +0530

    commited

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const app = express();
-let {people} = require('./data');
-app.set("json spaces", 2)
-
-app.get('/api/people', (re, res) =>{
+let { people } = require("./data");
+app.set("json spaces", 2);
+// static assests
+app.use(express.static("./methods-public"));
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+app.get("/api/people", (req, res) => {
   res.status(200).json({
-    sucess : true,
+    sucess: true,
     data: people,
-  })
-})
+  });
+});
 
+app.post("/login", (req, res) => {
+  const {name} = req.body;
+  if(name){
+    return res.status(200).send(`Welcome ${name}`)
+  }
+  res.status(401).send('Please provide the name');
+});
 
 app.listen(5000, () => {
   console.log("Server is listening to 5000...");
