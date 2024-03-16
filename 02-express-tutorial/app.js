5ba7aca3a6f4169ab8533557d8aec05f16ffaaeb
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-03-16 13:04:10 +0530

    commited

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,22 +1,20 @@
 const express = require("express");
 const app = express();
-
-
-const logger = (req, res, next) =>{
-  const method = req.method;
-  const url = req.url;
-  const time = new Date().getTime();
-  console.log(method,url,time);
-  // res.send('Testing');
-  next();
-}
-app.get('/' ,logger ,(req, res) =>{
-
-  res.send('Home');
-})
-app.get('/about',logger ,(req, res) =>{
-  res.send('About');
-})
+const logger = require("./logger");
+//uses only after api query
+app.use("/api", logger);
+app.get("/", (req, res) => {
+  res.send("Home");
+});
+app.get("/about", (req, res) => {
+  res.send("About");
+});
+app.get("/api/products", (req, res) => {
+  res.send("Products");
+});
+app.get("/api/items", (req, res) => {
+  res.send("items");
+});
 
 app.listen(5000, () => {
   console.log("Server is listening to 5000...");
