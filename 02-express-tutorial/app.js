77f4b5c62fd340a466e7b7bda0a20be0bfa3913f
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-03-16 15:47:51 +0530

    commited

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,30 +1,15 @@
 const express = require("express");
 const app = express();
-const logger = require("./logger");
-const authorize = require("./authorize");
-//uses only after api query
-// app.use("/api", logger);
-const { products } = require("./data");
+let {people} = require('./data');
+app.set("json spaces", 2)
 
-const morgan = require('morgan')
-app.use(morgan('tiny'))
-// app.use([logger, authorize]);
-// app.use(express.static("./public"))
-app.get("/", (req, res) => {
-  res.send("Home");
-});
-app.get("/about", (req, res) => {
-  res.send("About");
-});
-app.set("json spaces", 2);
+app.get('/api/people', (re, res) =>{
+  res.status(200).json({
+    sucess : true,
+    data: people,
+  })
+})
 
-app.get("/api/products", (req, res) => {
-  res.send(products);
-});
-app.get("/api/items", (req, res) => {
-  console.log(req.user);
-  res.send("items");
-});
 
 app.listen(5000, () => {
   console.log("Server is listening to 5000...");
