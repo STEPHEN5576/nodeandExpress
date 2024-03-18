53e69d94f00e25c30d14398eb3f5487b2e2dc8eb
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-03-18 11:45:40 +0530

    commited

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -4,21 +4,27 @@ let { people } = require("./data");
 app.set("json spaces", 2);
 // static assests
 app.use(express.static("./methods-public"));
+
+app.use(express.json);
 // parse form data
 app.use(express.urlencoded({ extended: false }));
 app.get("/api/people", (req, res) => {
-  res.status(200).json({
+  res.status(200).send({
     sucess: true,
     data: people,
   });
 });
 
+app.post('/api/people', (req, res) =>{
+  res.status(201).send('Sucess');
+})
+
 app.post("/login", (req, res) => {
-  const {name} = req.body;
-  if(name){
-    return res.status(200).send(`Welcome ${name}`)
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
   }
-  res.status(401).send('Please provide the name');
+  res.status(401).send("Please provide the name");
 });
 
 app.listen(5000, () => {
