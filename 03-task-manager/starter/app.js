b16cae0ad61b64dc05e673f72ffa3d1659a90911
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-03-18 16:51:05 +0530

    commited

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,11 +1,1 @@
 
-const express = require('express');
-
-const app = express();
-
- app.get('/hello' ,(req, res)=>{
-res.send('Task Manager app');
- })
-const port = 3000;
-
-app.listen(port,console.log(`server is listening on ${port}...`))
