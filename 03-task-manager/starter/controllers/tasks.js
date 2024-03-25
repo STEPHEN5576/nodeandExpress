ec73cd0e58d73efa4a957a55c10f743954c7c34c
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-03-25 18:24:02 +0530

    commited

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -20,7 +20,6 @@ const getTask =async (req, res) => {
   try{
     const{id: taskID} = req.params
     const task = await Task.findOne({_id:taskID})
-
     if(!task){
       return res.status(404).json({msg:`No task with id : ${taskID}`})
     }
@@ -32,8 +31,18 @@ const getTask =async (req, res) => {
 const UpdateTask = (req, res) => {
   res.send("update Task");
 };
-const deleteTask = (req, res) => {
-  res.send("Delete Task");
+
+const deleteTask = async (req, res) => {
+  try{
+const {id: taskID} = req.params;
+const task = await Task.findOneAndDelete({_id:taskID})
+if(!task){
+  res.status(404).send({msg: `Not found id: ${taskID}`})
+}
+res.status(200).json({ task });
+  }catch(error){
+
+  }
 };
 module.exports = {
   getAllTasks,
