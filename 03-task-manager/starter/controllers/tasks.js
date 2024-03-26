f9e2a403dcc7b46b43389e20eb8816e17b39f5ec
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-03-26 11:47:47 +0530

    commited

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -25,25 +25,45 @@ const getTask =async (req, res) => {
     }
     res.status(200).json({ task }); 
   }catch(error){
-    console.log(error);
+    res.status(404).json({msg: error})
   }
 };
-const UpdateTask = (req, res) => {
-  res.send("update Task");
-};
+
 
 const deleteTask = async (req, res) => {
   try{
 const {id: taskID} = req.params;
 const task = await Task.findOneAndDelete({_id:taskID})
 if(!task){
-  res.status(404).send({msg: `Not found id: ${taskID}`})
+  return res.status(404).send({msg: `Not found id: ${taskID}`})
 }
-res.status(200).json({ task });
+res.status(200).json({ task: null, status: 'success'});
   }catch(error){
-
+    res.status(404).json({ msg: error });
   }
 };
+
+const UpdateTask = async (req, res) => {
+  try{
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({_id: taskID}, req.body,{
+      new: true,
+      runValidators:true,
+    })
+
+    if (!task) {
+      return res.status(404).json({ msg: `Not found id: ${taskID}` });
+    }
+
+    res.status(200).json({task})
+
+    
+  }
+  catch(error){
+    res.status(500).json({msg: error})
+  }
+}; 
+
 module.exports = {
   getAllTasks,
   createTask,
