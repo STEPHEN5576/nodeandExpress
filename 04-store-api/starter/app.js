a5c34bf4f1590963d400ef57c9aa682ab24eb192
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-03-30 18:30:52 +0530

    commited

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 
+const connectDB =  require('./db/connect')
 
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -28,6 +29,7 @@ const port = process.env.PORT || 3000
 const start =  async ()=>{
  try{
   // connectDB
+  await connectDB(process.env.MONGO_URI)
   app.listen(port, console.log(`Server is listening to port: ${port}...`))
  }catch(error){
 
