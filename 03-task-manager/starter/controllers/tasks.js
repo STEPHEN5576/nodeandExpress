0576c766003d937d42b28296b05e375c8f7a6995
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-03-30 16:12:20 +0530

    commited

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,72 +1,65 @@
 const Task = require("../models/Task");
-const asyncWrapper = require('../middleware/async')
+const asyncWrapper = require("../middleware/async");
 
-
-const getAllTasks =asyncWrapper( async (req, res) => {
-  
-    const tasks = await Task.find({});
-    // res.status(200).json({ tasks });
-    // res.status(200).json({ tasks,data:tasks.length });
-    res
-      .status(200)
-      .json({ status: 'success', data: { tasks, nbHits: tasks.length } });
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({});
+  // res.status(200).json({ tasks });
+  // res.status(200).json({ tasks,data:tasks.length });
+  res
+    .status(200)
+    .json({ status: "success", data: { tasks, nbHits: tasks.length } });
 });
 
-const createTask =asyncWrapper( async (req, res) => {
-    const task = await Task.create(req.body);
-    res.status(201).json({ task });
-  
+const createTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
 });
 
-const getTask = asyncWrapper( async (req, res) => {
-  
-    const { id: taskID } = req.params;
-    const task = await Task.findOne({ _id: taskID });
-    if (!task) {
-      return res.status(404).json({ msg: `No task with id : ${taskID}` });
-    }
-    res.status(200).json({ task });
-  
+const getTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOne({ _id: taskID });
+  if (!task) {
+    const error = new Error({ msg: `No task with id : ${taskID}` });
+    return next(error);
+    // return res.status(404).json({ msg: `No task with id : ${taskID}` });
+  }
+  res.status(200).json({ task });
 });
 
-const deleteTask =asyncWrapper( async (req, res) => {
-  
-    const { id: taskID } = req.params;
-    const task = await Task.findOneAndDelete({ _id: taskID });
-    if (!task) {
-      return res.status(404).send({ msg: `Not found id: ${taskID}` });
-    }
-    res.status(200).json({ task: null, status: "success" });
-  
+const deleteTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndDelete({ _id: taskID });
+  if (!task) {
+    return res.status(404).send({ msg: `Not found id: ${taskID}` });
+  }
+  res.status(200).json({ task: null, status: "success" });
 });
 
-const UpdateTask =asyncWrapper( async (req, res) => {
+const UpdateTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-    const { id: taskID } = req.params;
-    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
-      new: true,
-      runValidators: true,
-    });
-
-    if (!task) {
-      return res.status(404).json({ msg: `Not found id: ${taskID}` });
-    }
-    res.status(200).json({ task });
-
+  if (!task) {
+    return res.status(404).json({ msg: `Not found id: ${taskID}` });
+  }
+  res.status(200).json({ task });
 });
 
-const editTask =asyncWrapper( async () => {
-    const { id: taskID } = req.params;
-    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
-      new: true,
-      runValidators: true,
-      overwrite: true,
-    });
-    if (!task) {
-      return res.status(404).json({ msg: `No task with the id: ${taskID}` });
-    }
-    return res.status(200).json({ task });
-  
+
+const editTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+    new: true,
+    runValidators: true,
+    overwrite: true,
+  });
+  if (!task) {
+    return res.status(404).json({ msg: `No task with the id: ${taskID}` });
+  }
+  return res.status(200).json({ task });
 });
 
 module.exports = {
@@ -75,4 +68,5 @@ module.exports = {
   getTask,
   UpdateTask,
   deleteTask,
+  editTask,
 };
